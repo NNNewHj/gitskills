3cc294f1e3341deb20a2513a0b2a39814808595c
Huang Jie <gahj0114@163.com>
2019-05-27 22:14:13 +0800

    a new java file

---
+++ Test.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+//: containers/Test.java
+// Framework for performing timed tests of containers.
+
+public class Test extends ATest {
+	
+	
+	
+	public static  void main(String[] args){
+		
+		StringBuilder stringBuilder = new StringBuilder(21);
+		
+		char[] a = new char[2147483400];
+		
+		for(int i = 0; i<stringBuilder.capacity();i++){
+			stringBuilder.append('a');
+		}
+		
+		StringBuilder stringBuilder1 = new StringBuilder(1000);
+		
+		for(int i = 0; i<stringBuilder1.capacity();i++){
+			stringBuilder.append('a');
+		}
+		
+		stringBuilder1.append(stringBuilder);
+		
+	}
+  
+}
